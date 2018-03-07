5f80a189e8bd8870742bbe87a99f315735723d83
Ong Ka-Shing <ongkashing@gmail.com>
2018-03-08 02:28:44 +0800

    Add CSS for Post component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
+import "./index.css";
+
 const Post = ({
   id,
   title,
+  score,
   upvote,
   downvote,
   onUpvoteClick,
   onDownvoteClick
 }) => {
   return (
-    <div className="post" data-id={id}>
-      <div className="post-title">{title}</div>
-      <div className="post-votes">
-        <div className="post-upvoteButton" onClick={onUpvoteClick} />
-        <div className="post-upvote">{upvote} Upvote </div>
-        <div className="post-downvoteButton" onClick={onDownvoteClick} />
-        <div className="post-upvote">{downvote} Downvote </div>
+    <div className="post">
+      <div className="leftPanel">
+        <div className="upvoteButton" onClick={onUpvoteClick} />
+        <div className="score">{score}</div>
+        <div className="downvoteButton" onClick={onDownvoteClick} />
+      </div>
+      <div className="rightPanel">
+        <div className="title">{title}</div>
+        <div className="votes">{upvote} upvotes, {downvote} downvotes </div>
       </div>
     </div>
   );
